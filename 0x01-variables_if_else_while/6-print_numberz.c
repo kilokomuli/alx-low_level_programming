6ff37af83f67e4bc3eb7ad13d4adef0f80cc3cab
kilokomuli <kilokoemma175@gmail.com>
2023-08-12 12:24:44 -0700

    changed 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,20 +2,19 @@
 
 /**
  * main - prints all single digit numbers of base 10 starting from 0,
- * followed by new line, using putchar
- * Return: Always 0 (success)
+ * followed by a new line, using putchar
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	int n;
 
-	for (n = 38; n < 48; n++)
+	for (n = 48; n < 58; n++)
 	{
 		putchar(n);
 	}
-	putchar("\n");
+	putchar('\n');
 	return (0);
-
 }
+
 
