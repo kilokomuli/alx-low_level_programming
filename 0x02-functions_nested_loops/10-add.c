d37dd3ec59f9707f154a666c2dd0364beb8a01f1
kilokomuli <kilokoemma175@gmail.com>
2023-08-16 14:13:50 -0700

    adde 100-times_table.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,5 +1,30 @@
 #include "main.h"
-/**
+/r(',');
+				_putchar(' ');
+				
+								prod = num * mult;
+								
+												if (prod <= 99)
+						_putchar(' ');
+										if (prod <= 9)
+						_putchar(' ');
+						
+										if (prod >= 100)
+					{
+											_putchar((prod / 100) + '0');
+																_putchar(((prod / 10)) % 10 + '0');
+																				}
+				else if (prod <= 99 && prod >= 10)
+					{
+											_putchar((prod / 10) + '0');
+															}
+				_putchar((prod % 10) + '0');
+							}
+			_putchar('\n');
+					}
+	}
+}
+**
  * add - Adds two integers.
  * @num1: The first integer to be added.
  * @num2: The second integer to be added.
@@ -9,4 +34,4 @@
 int add(int num1, int num2)
 {
 	return (num1 + num2);
-}
+ 
