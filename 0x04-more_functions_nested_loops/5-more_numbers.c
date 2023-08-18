80429edd0129f3ffb1da3726879f9191762f25f7
kilokomuli <kilokoemma175@gmail.com>
2023-08-18 14:34:35 -0700

    task 5 print the numbers 10 times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times the numbers
+ * from 0 to 14
+ * followed by a new line
+ */
+
+void more_numbers(void)
+{
+	int e, k;
+
+	for (e = 0; e <= 10; e++)
+	{
+		for (k = 0; k <= 14; k++)
+		{
+			if (k >= 10)
+			{
+				_putchar(k / 10 + '0');
+			}
+			_putchar(k % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
