5a8359683744c42f2225d39568ed05a500dfc054
kilokomuli <kilokoemma175@gmail.com>
2023-08-18 18:23:24 -0700

    updated task9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,7 +13,7 @@ int main(void)
 {
 	int i;
 
-	for (i = 0;  i < 100; i++)
+	for (i = 1;  i <= 100; i++)
 	{
 		if ((i % 3 == 0) && (i % 5 == 0))
 		{
@@ -29,8 +29,10 @@ int main(void)
 		}
 		else
 		{
-			printf(" %d", i);
+			printf("%d", i);
 		}
+		if (i != 100)
+			printf(" ");
 	}
 	printf("\n");
 	return (0);
