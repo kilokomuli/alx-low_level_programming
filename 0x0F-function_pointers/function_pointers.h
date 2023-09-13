b39bfe2866808adf56a220961bb51cabedd3edc7
root <kilokoemma175@gmail.com>
2023-09-13 07:13:12 -0700

    print name

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,10 @@
+#ifndef FUNCTION_POINTERS_H
+#define FUNTCION_POINTERS_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+int int_index(int *array, int size, int (*cmp)(int));
+#endif
