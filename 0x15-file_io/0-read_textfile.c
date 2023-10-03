2128aad442d07bf687ebfd41707d858947e7726f
kilokomuli <kilokoemma175@gmail.com>
2023-10-03 06:56:12 -0700

    solving errors

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -16,7 +16,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (filename == NULL)
 		return (0);
-	
+
 	file = open(filename, O_RDONLY);
 	buffer = malloc(sizeof(char) * letters);
 
