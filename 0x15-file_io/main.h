bf9452c6a5db0f35af32c25b537545edeb13cf28
kilokomuli <kilokoemma175@gmail.com>
2023-10-03 07:20:40 -0700

    craete afile

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <fcntl.h>
 #include <sys/stat.h>
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
