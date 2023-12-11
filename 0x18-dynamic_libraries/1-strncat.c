15ddeb9b7f034651117be8fdf5dd9b30ed6174e9
kilokomuli <kilokoemma175@gmail.com>
2023-12-11 10:45:26 -0800

    task 0 and 1

---
+++ 0x18-dynamic_libraries/1-strncat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenate strings, defining the size of second string.
+ * @dest: string with concatenation
+ * @src: string to be concatenated
+ * @n: size of second string
+ * Return: Always 0.
+ */
+
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	int len = strlen(dest);
+
+	for (i = 0; i < n && *src != '\0'; i++)
+	{
+		dest[len + i] = *src;
+		src++;
+	}
+	dest[len + i] = '\0';
+	return (dest);
+}
